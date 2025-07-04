e5ca1d237b5ededd722a41b8ae335444d73114d7
user <user@DESKTOP-J2VMFB4>
2025-07-04 17:48:00 +0900

    사용자 숙소 조회 데이터베이스에서 가져오게 수정

--- DUGAZA/src/main/java/kr/spring/house/controller/HouseController.java
+++ DUGAZA/src/main/java/kr/spring/house/controller/HouseController.java
@@ -1,17 +1,40 @@
 package kr.spring.house.controller;
 
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.spring.house.service.HouseService;
+import kr.spring.house.vo.HouseVO;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
 @RequestMapping("/house")
 public class HouseController {
+	
+	@Autowired
+	private HouseService houseService;
+
 	@GetMapping("")
-	public String accommodationMain() {
+	public String accommodationMain(Model model) {
+		log.debug("<<accommodationMain 호출>>");
+
+		Map<String,Object> map = new HashMap<String,Object>();
+		// 현재는 전체 목록을 가져오기 위해 start와 end를 임의로 설정
+		// 실제 구현에서는 페이징 처리 로직이 필요
+		map.put("start", 1);
+		map.put("end", 10);
+
+		List<HouseVO> list = houseService.selectList(map);
+		model.addAttribute("list", list);
+
 		return "views/sample/accommodation";
 	}
 }
--- DUGAZA/src/main/java/kr/spring/house/dao/HouseMapper.java
+++ DUGAZA/src/main/java/kr/spring/house/dao/HouseMapper.java
@@ -5,12 +5,10 @@ import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import kr.spring.tour.vo.TourVO;
+import kr.spring.house.vo.HouseVO;
 
 @Mapper
 public interface HouseMapper {
-	// 숙소 총 개수
-	public Integer selectRowCount();
 	// 숙소 목록
-	public List<TourVO> selectList(Map<String,Object> map);
+	public List<HouseVO> selectList(Map<String,Object> map);
 }
