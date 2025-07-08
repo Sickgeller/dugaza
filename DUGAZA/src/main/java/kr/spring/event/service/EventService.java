e3d8562328372a28287ad87b9093595ae3e937ff
SIST <SIST@DESKTOP-AI8LLNL>
2025-07-08 18:22:02 +0900

    행사 추가 / 페이지는 구성중

---
+++ DUGAZA/src/main/java/kr/spring/event/service/EventService.java
@@ -0,0 +1,15 @@
+package kr.spring.event.service;
+
+import java.util.List;
+import java.util.Map;
+
+import kr.spring.event.vo.EventVO;
+import kr.spring.tour.vo.TourVO;
+
+public interface EventService {
+	// 행사 총 개수
+	public Integer selectRowCount();
+	// 행사 목록
+	public List<TourVO> selectList(Map<String,Object> map);
+	public EventVO selectEvent(Long id);
+}
