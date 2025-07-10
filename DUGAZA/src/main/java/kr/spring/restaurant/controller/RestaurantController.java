b750e5be8caa80c13b49d98106b5c666fdb5b861
SIST <SIST@DESKTOP-AI8LLNL>
2025-07-10 17:51:30 +0900

    식당 리뷰 추가

--- DUGAZA/src/main/java/kr/spring/restaurant/controller/RestaurantController.java
+++ DUGAZA/src/main/java/kr/spring/restaurant/controller/RestaurantController.java
@@ -5,14 +5,26 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.servlet.http.HttpSession;
+import kr.spring.auth.security.CustomUserDetails;
+import kr.spring.house.vo.HouseVO;
+import kr.spring.member.vo.MemberVO;
 import kr.spring.restaurant.service.RestaurantService;
 import kr.spring.restaurant.vo.RestaurantVO;
+import kr.spring.review.base.service.BaseReviewService;
+import kr.spring.review.base.service.ReviewStatisticsService;
+import kr.spring.review.base.vo.BaseReviewVO;
+import kr.spring.review.base.vo.ReviewStatisticsVO;
 import kr.spring.tour.vo.TourVO;
 import kr.spring.tour.vo.TouristAttractionVO;
 import kr.spring.util.PagingUtil;
@@ -25,8 +37,13 @@ public class RestaurantController {
 
 	@Autowired
 	private RestaurantService restaurantService;
-	
-	
+
+	@Autowired
+	private BaseReviewService baseReviewService;
+
+	@Autowired
+	private ReviewStatisticsService reviewStatisticsService;
+
 	@GetMapping("")
 	public String restaurantMain(@RequestParam(name = "pageNum", defaultValue="1") int pageNum,
 			@RequestParam(name = "keyword", defaultValue = "") String keyword,
@@ -55,11 +72,33 @@ public class RestaurantController {
 
 	// 항목 자세히 보기
 	@GetMapping("/detail")
-	public String restaurantDetail(@RequestParam(name = "id") Long id, Model model) {
-		RestaurantVO vo = restaurantService.selectRestaurant(id);
+	public String restaurantDetail(@RequestParam(name = "contentId") Long contentId, Model model) {
+		// 식당 정보
+		RestaurantVO vo = restaurantService.selectRestaurant(contentId);
+		// 식당별 리뷰 목록
+		List<BaseReviewVO> reviewList = baseReviewService.getHouseReviews(contentId, 1, 10);
+		// 식당별 리뷰 통계
+		ReviewStatisticsVO status = reviewStatisticsService.getReviewStatisticsByHouse(contentId);
 
 		model.addAttribute("info",vo);
+		model.addAttribute("reviewList",reviewList);
+		model.addAttribute("status", status);
 
 		return "views/restaurant/restaurant-detail";
+	}
+
+	// 리뷰 작성
+	@PostMapping("/saveReview")
+	public String saveReview(@ModelAttribute BaseReviewVO reviewDTO, HttpSession session) {
+		
+		MemberVO member = (MemberVO) session.getAttribute("member");
+		reviewDTO.setMemberId(member.getMemberId());
+		reviewDTO.setStatus(1);
+		reviewDTO.setContentTypeId(39L); // 음식점 타입 ID
+
+		baseReviewService.writeReview(reviewDTO);
+		log.debug("<<리뷰 작성>> 사용자 id : {}", member.getMemberId());
+
+		return "redirect:/restaurant/detail?contentId=" + reviewDTO.getContentId();
 	}
 }
